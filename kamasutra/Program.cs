31383494d313c7c5571fe74409a496226cfc8aac
Kyle <Kyle@DESKTOP-9P3CFRI>
2022-10-06 23:33:47 +0100

    Add project files.

---
+++ kamasutra/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Welcome to the karmasutra, please press enter but only if your 18 or over");
+Console.ReadKey();
+
+Console.WriteLine("alright so basically you have a boring sex life and youve come here to find new positions, bad news is you are basic and this book is useless either way please pick from the 4 options below");
+Console.WriteLine("\ta - boring");
+Console.WriteLine("\tb - gets the job done");
+Console.WriteLine("\tc - living the higher life");
+Console.WriteLine("\td - your parnter will cherish you to the end of days");
+Console.Write("Your option? ");
+
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine("Saddling, now this is so shit and wont get you anywhere in life no wonder your single.");
+        break;
+    case "b":
+        Console.WriteLine("The Speedbump, now i picked this one because the name is cool! its just like driving a car.");
+        break;
+    case "c":
+        Console.WriteLine("Reverse Cowgirl, this one is okay, it defitently is handy and quick to do the only downside is you cant see their tits");
+        break;
+    case "d":
+        Console.WriteLine("Shower, now every girl loves this, when theyre in the shower and you just jump in and fuck them its great 10/10");
+        break;
+}
+Console.Write("okay fabulous now go get steamy with your partner");
+Console.ReadKey();
